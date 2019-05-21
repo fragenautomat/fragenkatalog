e78716b4f51d44c136c07b2723fbdea77eb86328
PhilippMatthes <github@philippmatth.es>
2019-05-21 10:57:13 +0200

    Integrate base styling and mapping

--- fragenkatalog/fragenkatalog/views.py
+++ fragenkatalog/fragenkatalog/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
 
 def index(request):
-    return HttpResponse("Hello Index!")
+    return TemplateResponse(request, "index.html")
