a709d078acf860507886bfa65bd76289887cb1aa
PhilippMatthes <github@philippmatth.es>
2019-07-09 12:01:33 +0200

    Change forms

--- fragenkatalog/fragenkatalog/questions/forms.py
+++ fragenkatalog/fragenkatalog/questions/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class NewQuestionForm(forms.Form):
-    description = forms.CharField(label="Description of the task", max_length=1000)
-    solution = forms.CharField(label="Solution of the task", max_length=1000)
+    description = forms.CharField(label="Description of the task", max_length=10000)
+    solution = forms.CharField(label="Solution of the task", max_length=10000)
     image = forms.ImageField(required=False)
